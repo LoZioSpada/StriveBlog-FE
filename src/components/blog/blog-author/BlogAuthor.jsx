6a85d20a1a3ec3699055a4295eea7ee2fbb2e129
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-11-22 20:38:09 +0100

    StriveBlog-FE
    - Aggiunta fetch per che collega la repository "StriveBlog (backend)" a questa repo di frontend;
    - Modificato il componente "BlogItem" per far funzionare il componente "BlogAuthor";

--- src/components/blog/blog-author/BlogAuthor.jsx
+++ src/components/blog/blog-author/BlogAuthor.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Image, Row } from "react-bootstrap";
 import "./styles.css";
 
-const BlogAuthor = props => {
-  const { name, avatar } = props;
+export default function BlogAuthor (){
+  const [authors, setAuthors] = useState([])
+
+  const makeAPICall = async () => {
+    try{
+      const response = await fetch(
+        `http://localhost:3030/api/authors/655cf4c77a4e03b9930a90e8`
+      )
+
+      if(!response.ok){
+        throw new Error(`HTTP ERROR! Status: ${response.status}`)
+      }
+
+      const data = await response.json()
+      setAuthors(data)
+    } catch(error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    makeAPICall()
+  }, [])
+
   return (
     <Row>
-      <Col xs={"auto"} className="pe-0">
-        <Image className="blog-author" src={avatar} roundedCircle />
+      <Col xs={"auto"} className="pe-0" key={authors._id}>
+        <Image className="blog-author" src={authors.avatar} roundedCircle />
       </Col>
       <Col>
         <div>di</div>
-        <h6>{name}</h6>
+        <h6>{authors.name} {authors.surname}</h6>
       </Col>
     </Row>
   );
 };
-
-export default BlogAuthor;
