91ca120c988cf66017a5efddc8e8e8084932732a
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-11-26 18:42:34 +0100

    StriveBlog-FE
    - Aggiornati altri componenti del frontend con l'aggiunta delle eventuali fetch;

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,10 +1,33 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
-import posts from "../../../data/posts.json";
+import React, { useState } from "react";
+import { Col, Row, Spinner } from "react-bootstrap";
 import BlogItem from "../blog-item/BlogItem";
 
-const BlogList = props => {
-  return (
+const BlogList = (props) => {
+
+  const { query, result } = props;
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const getPosts = async () => {
+    try{
+      const response = await fetch("http://localhost:3030/api/blogPosts")
+      if(!response.ok){
+        throw new Error(`HTTP error: ${response.status}`)
+      }
+      const data = await response.json()
+      setPosts(data)
+    } catch(error){
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return loading ? (
+    <div className="d-flex mt-5">
+      <Spinner animation="border" variant="primary" className="mx-auto" />
+    </div>
+  ) : (
     <Row>
       {posts.map((post, i) => (
         <Col
@@ -14,7 +37,7 @@ const BlogList = props => {
             marginBottom: 50,
           }}
         >
-          <BlogItem key={post.title} {...post} />
+          <BlogItem key={post.title} {...post} loading={loading}/>
         </Col>
       ))}
     </Row>
--- src/components/navbar/BlogNavbar.jsx
+++ src/components/navbar/BlogNavbar.jsx
@@ -1,29 +1,67 @@
 import React from "react";
-import { Button, Container, Navbar } from "react-bootstrap";
+import { Button, Col, Container, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import "./styles.css";
-const NavBar = props => {
+
+
+const NavBar = (props) => {
+
+  const { query, setQuery, setResult } = props;
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`http://localhost:3030/blogPosts?title=${query}`)
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      const data = await response.json()
+      setResult(data.blogPosts)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <Navbar expand="lg" className="blog-navbar" fixed="top">
       <Container className="justify-content-between">
-        <Navbar.Brand as={Link} to="/">
-          <img className="blog-navbar-brand" alt="logo" src={logo} />
-        </Navbar.Brand>
+        <Col xs={6} md={4} className='me-2 mb-4'>
+          <Navbar.Brand as={Link} to="/">
+            <img className="blog-navbar-brand" alt="logo" src={logo} />
+          </Navbar.Brand>
+        </Col>
 
-        <Button as={Link} to="/new" className="blog-navbar-add-button bg-dark" size="lg">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-plus-lg"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
-          </svg>
-          Nuovo Articolo
-        </Button>
+        <Col md={4} xs={{ span: 12, order: 2 }} className="order-sm-2">
+          <Form className="d-flex">
+            <Form.Control
+              type="text"
+              value={query}
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <Button variant="outline-success" onClick={handleSearch}>
+              Search
+            </Button>
+          </Form>
+        </Col>
+
+        <Col xs={5} md={{ span: 1, order: 2 }}>
+          <Button as={Link} to="/new" className="blog-navbar-add-button bg-dark" size="lg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-plus-lg"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
+            </svg>
+            Nuovo Articolo
+          </Button>
+        </Col>
       </Container>
     </Navbar>
   );
